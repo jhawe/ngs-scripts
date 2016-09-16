d48bf82f424644f28008238c7b6dee7d1eeddc7d
Johann Hawe <johann.hawe@gmx.de>
2016-09-16 11:08:49 +0200

    test

---
+++ shell/process_sam.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+###
+# currently just a test for github.
+# Needs samtools v1.3.*
+# Assumes samtools has been installed to the personal library
+###
+
+###
+# Vars
+# TODO: proper usage informaiton and paramter parsing
+###
+sam=$1
+outdir=$2
+if [ "$#" == "3" ] ; then
+ prefix=${outdir}/$3
+elif
+ prefix=${outdir}/$(basename ${sam} .sam)
+fi
+
+# set samtools path
+st=~/bin/samtools
+
+###
+# start the processing
+###
+st view -bS -q 30 ${sam} \
+| st sort - > ${prefix}.bam
+st index ${prefix}.bam
